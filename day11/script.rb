39291c951a7c1c5c622e32f2396a955338b1c772
Gabe Kahen <gabe.kahen@onvoy.com>
2017-12-12 09:45:34 -0700

    Solution for day 11 / pt 2

--- day11/script.rb
+++ day11/script.rb
@@ -9,42 +9,49 @@ class Hexagon
 #   \    /
 #  y +--+  x
 #   / z  \
-  attr_accessor :x, :y, :z
+  attr_accessor :x, :y, :z, :max_distance
 
   def initialize(x = 0, y = 0, z = 0)
     @x = x
     @y = y
     @z = z
+    @max_distance = self.distance
   end
 
   def nw
     @y -= 1
     @z -= 1
+    @max_distance = self.distance if @max_distance < self.distance
   end
 
   def se
     @y += 1
     @z += 1
+    @max_distance = self.distance if @max_distance < self.distance
   end
 
   def ne
     @z += 1
     @x += 1
+    @max_distance = self.distance if @max_distance < self.distance
   end
 
   def sw
     @z -= 1
     @x -= 1
+    @max_distance = self.distance if @max_distance < self.distance
   end
 
   def n
     @y -= 1
     @x += 1
+    @max_distance = self.distance if @max_distance < self.distance
   end
 
   def s
     @y += 1
     @x -= 1
+    @max_distance = self.distance if @max_distance < self.distance
   end
 
   def print
@@ -66,3 +73,4 @@ instructions = data.chomp.split(',')
 instructions.each { |i| h.send(i) }
 
 printf("Distance: %d\n", h.distance)
+printf("Max Distance: %d\n", h.max_distance)
